27c08e4f11d6f8295144e9e2650f87ae18d266ea
haohao6683 <852721803@qq.com>
2020-08-05 11:16:37 +0800

    [todo-list][Draymond&Henry] Feature: add a router to switch different path.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import TodoForm from "./containers/todoForm"
 import TodoListComponent from "./containers/todoList"
-// import todoFormComponent from "./component/todoForm/index"
+import DoneTodoListContainer from './containers/doneTodoList'
+import { HashRouter, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-          <TodoForm />
-          <TodoListComponent />
+          <TodoForm/>
+          <HashRouter>
+            <div>
+             <Link to='/'>go to todoList page</Link><br/>
+             <Link to='/done'>go to done todoList page</Link>
+            </div>
+            <Switch>
+              <Route exact path='/' component={TodoListComponent}/>
+              <Route exact path='/done' component={DoneTodoListContainer}/>
+            </Switch>
+          </HashRouter>
       </div>
     </div>
   );
